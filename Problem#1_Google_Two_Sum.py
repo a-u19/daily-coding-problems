534c6a2331ccaec2353a073324770340f004be59
a-u19 <uarjun@hotmail.co.uk>
2024-10-03 15:25:31 +0100

    Finished Problem #1
    Finished Problem #1 - Google - Two Sum

---
+++ Problem#1_Google_Two_Sum.py
@@ -0,0 +1,18 @@
+'''
+Problem #1
+
+This problem was recently asked by Google.
+
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+'''
+# if nums only contained positive numbers, then all numbers greater than k could be removed but question doesn't specify
+def main(target: int, nums: list):
+    for num in nums:
+        difference = target - num
+        if difference in nums:
+            return True
+    return False
+
+print(main(17, [9, 15, 3, 7]))
