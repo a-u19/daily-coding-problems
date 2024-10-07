b38a27d821ba0780fccc14242648b057276aef2c
a-u19 <85358563+a-u19@users.noreply.github.com>
2024-10-07 14:06:58 +0100

    Update Problem#1_Google_Two_Sum.py
    Expanded to summing multiple numbers, not just two. Included test cases too.

--- Problem#1_Google_Two_Sum.py
+++ Problem#1_Google_Two_Sum.py
@@ -8,11 +8,34 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
 '''
 # if nums only contained positive numbers, then all numbers greater than k could be removed but question doesn't specify
-def main(target: int, nums: list):
-    for num in nums:
+def main(target:int, nums:list) -> bool:
+    for i, num in enumerate(nums):
         difference = target - num
-        if difference in nums:
+        if difference in nums[:i] + nums[i+1:]:
             return True
     return False
 
-print(main(17, [9, 15, 3, 7]))
+def main_three_numbers(target:int, nums:list) -> bool:
+    for i, num in enumerate(nums):
+        difference = target - num
+        if main(target = difference, nums = nums[:i] + nums[i+1:]):
+            return True
+    return False
+
+def main_n_numbers(target:int, nums:list, n:int) -> bool:
+    if n == 2:
+        return main(target, nums)
+    
+    for i, num in enumerate(nums):
+        difference = target - num
+        if main_n_numbers(target = difference, nums = nums[:i] + nums[i+1:], n = n - 1):
+            return True
+    return False
+
+testcase1 = [17, [9, 15, 3, 7]]
+testcase2 = [20, [10, 3, 5, 7]]
+testcase3 = [15, [1,2,3,4,5]]
+print(f"Test case: target = {testcase1[0]}, nums = {testcase1[1]} returns {main(testcase1[0], testcase1[1])}")
+print(f"Test case: target = {testcase2[0]}, nums = {testcase2[1]} returns {main(testcase2[0], testcase2[1])}")
+print(f"Test case: target = {testcase2[0]}, nums = {testcase2[1]} returns {main_three_numbers(testcase2[0], testcase2[1])}")
+print(f"Test case: target = {testcase3[0]}, nums = {testcase3[1]} returns {main_n_numbers(testcase3[0], testcase3[1], 4)}")
