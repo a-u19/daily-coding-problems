de6747679cc9d35094d996391247a9578640ed92
Arjun Uthayathasan <q659406@sfeu.bmw.corp>
2024-10-21 08:35:59 +0100

    Added to Problem #12

--- Problem#12_Amazon_Staircase_N_Steps.py
+++ Problem#12_Amazon_Staircase_N_Steps.py
@@ -10,10 +10,6 @@ For example, if N is 4, then there are 5 unique ways:
 •	1, 1, 2
 •	2, 2
 
-Part 2:
-What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers
- X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
-
 Steps - Ways
 1 - 1
 2 - 2
@@ -41,11 +37,32 @@ def fib_with_dict(n:int) -> int:
     return fib_dict[n]
 
 testcases = [1,2,3,4,5,6,7]
+testcases2 = [n for n in range(1,30)]
 start_time = time.time()
-for testcase in testcases:
+for testcase in testcases2:
     print(f"If there are {testcase} number of steps, then there are {fib_with_no_dict(testcase)} number of ways to climb up.")
 print(f"Without a dictionary this took {time.time() - start_time}")
 start_time = time.time()
-for testcase in testcases:
+for testcase in testcases2:
     print(f"If there are {testcase} number of steps, then there are {fib_with_dict(testcase)} number of ways to climb up.")
 print(f"With a dictionary this took {time.time() - start_time}")
+
+"""
+Part 2:
+What if, instead of being able to climb 1 or 2 steps at a time, you could climb any number from a set of positive integers
+ X? For example, if X = {1, 3, 5}, you could climb 1, 3, or 5 steps at a time.
+ 
+ Steps - Ways
+ 1 - 1
+ 2 - 1
+ 3 - 2
+ 4 - 3
+ 5 - 1, 1, 1, 1, 1;
+    1, 1, 3;
+    1, 3, 1;
+    3, 1, 1;
+    5;
+ 5 - 5 ways
+ 
+ same fibonacci sequence so same functions can be used
+"""
